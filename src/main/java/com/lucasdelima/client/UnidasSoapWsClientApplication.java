6bb5c750ae2fc905ee7ea773a62d26854cc13f13
grizzlyPi <lucasrodriguesdelima@rocketmail.com>
2020-05-11 19:30:42 -0300

    Created project

---
+++ src/main/java/com/lucasdelima/client/UnidasSoapWsClientApplication.java
@@ -0,0 +1,13 @@
+package com.lucasdelima.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidasSoapWsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidasSoapWsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasdelima/client/UnidasSoapWsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdelima.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidasSoapWsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
